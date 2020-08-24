05c43509d245e568d86e362ec25c18d910bf0b0e
vishal-oms <53806871+vishal-oms@users.noreply.github.com>
2020-08-24 20:43:39 +0530

    Add project files.

---
+++ OnlineShoppingSample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OnlineShoppingSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
