c25a0feb2259ee3b22619c3251e0c78a1c49d899
vishal-oms <53806871+vishal-oms@users.noreply.github.com>
2020-08-24 21:47:49 +0530

    Finish : All unit test added

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -19,5 +19,44 @@ namespace UnitTestProject1
             Assert.IsTrue(result1.Contains("Slip generated..."));
             Assert.IsTrue(result2.Contains("Commission generated..."));
         }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Arrange
+            IAgentContract concreteAgent = new ConcreteAgent();
+            Book entity = new Book(concreteAgent);
+            //Act
+            string result1 = entity.GenerateDulpicateSlip();
+            string result2 = concreteAgent.Commission();
+            //Assert
+            Assert.IsTrue(result1.Contains("Duplicate Slip generated..."));
+            Assert.IsTrue(result2.Contains("Commission generated..."));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Arrange
+            IEMail concreteEmail = new ConcreteEMail();
+            ConcreteMember entity = new ConcreteMember(concreteEmail);
+            //Act
+            string result1 = entity.ActivateMember();
+            string result2 = concreteEmail.SendEmail();
+            //Assert
+            Assert.IsTrue(result1.Contains("Member activated..."));
+            Assert.IsTrue(result2.Contains("Email sent..."));
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            //Arrange
+            ConcreteVideo entity = new ConcreteVideo();
+            //Act
+            string result1 = entity.GeneratePackagingSlip();
+            //Assert
+            Assert.IsTrue(result1.Contains("Packaging Slip generated..."));
+        }
     }
 }
