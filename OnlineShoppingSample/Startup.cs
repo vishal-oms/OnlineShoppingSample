46f9e122bcb5b59e9a946477f2d5cfb79933b0a0
vishal-oms <53806871+vishal-oms@users.noreply.github.com>
2020-08-24 21:40:29 +0530

    Endpoint configured

--- OnlineShoppingSample/Startup.cs
+++ OnlineShoppingSample/Startup.cs
@@ -49,7 +49,7 @@ namespace OnlineShoppingSample
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Assignment}/{id?}");
             });
         }
     }
