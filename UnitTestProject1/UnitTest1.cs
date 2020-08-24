d05dbacfc0761b4e22f83d48e5ac7c78bef27283
vishal-oms <53806871+vishal-oms@users.noreply.github.com>
2020-08-24 21:46:00 +0530

    Fisrt unit test added

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using WebApplication4.DomainModels; 
+using WebApplication4.Services;
 namespace UnitTestProject1
 {
     [TestClass]
@@ -8,6 +9,15 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestMethod1()
         {
+            //Arrange
+            IAgentContract concreteAgent = new ConcreteAgent();
+            ConcreteProduct entity = new ConcreteProduct(concreteAgent);
+            //Act
+            string result1 = entity.GenerateSlip();
+            string result2 = concreteAgent.Commission();
+            //Assert
+            Assert.IsTrue(result1.Contains("Slip generated..."));
+            Assert.IsTrue(result2.Contains("Commission generated..."));
         }
     }
 }
